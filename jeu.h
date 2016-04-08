83c4222e796d87ac04bd95b450504d5b05d05f91
cutty853 <cutty853@gmail.com>
2016-04-08 14:50:12 +0200

    ajout du proto de play(), mise a jour commune

--- jeu.h
+++ jeu.h
@@ -3,3 +3,4 @@ int menu(SDL_Surface *ecran);
 void charge_niveau (SDL_Surface *ecran);
 void barre_vie(SDL_Surface* ecran, _vaisseau v_joueur, SDL_Rect pos_barre);
 void barre_bouclier(SDL_Surface* ecran, _vaisseau v_joueur, SDL_Rect pos_barre);
+void play(SDL_Surface *ecran);
