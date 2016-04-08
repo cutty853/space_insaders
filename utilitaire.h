2001b0f203002ead49670aeaf6b2a5c65d11d1eb
cutty853 <cutty853@gmail.com>
2016-04-08 14:48:53 +0200

    Update utilitaire.h

--- utilitaire.h
+++ utilitaire.h
@@ -1,3 +1,4 @@
 void test_surface(SDL_Surface* surface, int ref_err);
+void init_pos(SDL_Rect* position, int x, int y);
 void pause();
 void degrade(_degrade prop_deg, SDL_Surface *ecran, SDL_Rect pos_degrade);
