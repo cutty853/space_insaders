1f0fbdc4fc7f6eccd8fcf4ccb99d3186ed25f851
Nagasakie <RemiPavan@live.fr>
2016-04-08 14:51:26 +0200

    mise à jour commune

--- jeu.h
+++ jeu.h
@@ -1,6 +1,9 @@
 void cadre(SDL_Surface *ecran, int pos_x, int pos_y);
 int menu(SDL_Surface *ecran);
 void charge_niveau (SDL_Surface *ecran);
-void barre_vie(SDL_Surface* ecran, _vaisseau v_joueur, SDL_Rect pos_barre);
-void barre_bouclier(SDL_Surface* ecran, _vaisseau v_joueur, SDL_Rect pos_barre);
+
+///Déclaration des fonctions du joueurs:
+void barre_vie_joueur(SDL_Surface* ecran, _vaisseau v_joueur);
+void barre_bouclier_joueur(SDL_Surface* ecran, _vaisseau v_joueur);
+
 void play(SDL_Surface *ecran);
