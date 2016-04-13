2f074f15297d76e037718e21c348a850e3f943e0
Nagasakie <RemiPavan@live.fr>
2016-04-13 16:13:03 +0200

    MAJ structure vaisseau
    ajout des sdl_surface
    ajout d'un état VIDE dans _charge

--- structure.h
+++ structure.h
@@ -9,22 +9,26 @@ typedef struct {
     _sens sens;
 } _degrade;
 
-typedef enum {MORT, BAS, MOYEN, HAUT} _charge;
+typedef enum{JOUEUR, IA} _intelligence;
+typedef enum {VIDE, BAS, MOYEN, HAUT} _charge;
 typedef enum {TIR_LASER, OBUS, RAYON_LASER} _arme;
 //typedef enum {CAPA1, CAPA2, CAPA3} _capacite;
 
 typedef struct {
     int poid;
-    double vitesse; // En pixel par seconde
+    int vitesse; // En pixel par seconde
     int acceleration; // En prixel par seconde*pow(-2)
     int vitesse_max; // En pixel par seconde
+    _intelligence intelligence;
     _charge bouclier;
     _charge vie;
     _arme arme;
     int vitesse_rotation;
     float angle;
+    int etat_rotation;
     //_capacite capacite;
     SDL_Rect position;
+    SDL_Surface *sprite;
 } _vaisseau;
 
 typedef struct {
