9879bd64ab0df27e9186cadc7ba413b899b31418
cutty853 <cutty853@gmail.com>
2016-06-02 22:21:21 +0200

    Lancement du jeu fonctionnel

--- moteur_physique.h
+++ moteur_physique.h
@@ -2,10 +2,10 @@ int col_point_boite (_point *x, SDL_Rect *boite);
 int col_point_cercle (int x, int y, _cercle* c);
 int col_boite_boite (SDL_Rect *repere, SDL_Rect *boite);
 int col_cercle_cercle (_cercle *c1, _cercle *c2);
-int col_polygone_cercle (_polygone *p, _cercle* c)
+int col_polygone_cercle (_polygone *p, _cercle* c);
 int col_polygone_point (_polygone *p, _point* x);
-int col_aabb_cercle (SDL_Rect* aabb, _cercle *c)
-void init_hitbox(_hitbox* h, int xCercle, int yCercle, int rCercle, int nb_points, int xAABB, int yAABB, int wAABB, int hAABB)
+int col_aabb_cercle (SDL_Rect* aabb, _cercle *c);
+void init_hitbox(_hitbox* h, int xCercle, int yCercle, int rCercle, int nb_points, int xAABB, int yAABB, int wAABB, int hAABB);
 void calcul_pos_hitbox_vaisseau(_vaisseau *v);
 void calcul_pos_hitbox_tir(_tir* t);
 void transform_aabb_polygone(SDL_Rect* aabb, _polygone *p);
